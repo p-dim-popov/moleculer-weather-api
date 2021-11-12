4b78365e9e0689bf6891415e0bf7d4de59e2d171
Peter Popov <p.dim.popov@gmail.com>
2021-11-12 22:51:39 +0200

    feat(typeorm): add db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB__HOST'),
+        port: configService.get('DB__PORT'),
+        username: configService.get('DB__USERNAME'),
+        password: configService.get('DB__PASSWORD'),
+        database: configService.get('DB__DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
