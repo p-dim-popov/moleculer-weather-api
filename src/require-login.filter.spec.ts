e2f95423e7778cb8e86b1866dc20a99460c16265
Peter Popov <p.dim.popov@gmail.com>
2021-11-18 17:28:04 +0200

    refactor: remove useless tests

--- src/require-login.filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RequireLoginFilter } from './require-login.filter';
-
-describe('RedirectToLoginFilter', () => {
-  it('should be defined', () => {
-    expect(new RequireLoginFilter()).toBeDefined();
-  });
-});
